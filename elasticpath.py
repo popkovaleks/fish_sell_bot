f95ef63e8f5aed42c6eaf8d36be3bde95ddfad3a
Aleksandr <popkov.aleks.v@yandex.ru>
2023-03-24 23:56:06 +0500

    telegram bot

---
+++ elasticpath.py
@@ -0,0 +1,166 @@
+import requests
+import json
+import time
+
+
+def access_token_is_expired():
+    try:
+        with open("access_data.json", "r") as access_file:
+            access_data = json.loads(access_file.read())
+            if access_data["expires"] < time.time():
+                return True
+            return False
+    except FileNotFoundError:
+        return True
+
+
+def get_access_token():
+    if access_token_is_expired() is True:
+        url = "https://api.moltin.com/oauth/access_token"
+
+        payload='client_id=GzfRZv0lswGiw2D56fyxQ8Abw8i2m9s9HJU1xHTgQU&client_secret=MTo6d1YMLqkqLH3CrJWYsz42DpndKJwkCyJP3dWBQT&grant_type=client_credentials'
+        headers = {
+        'accept': 'application/json',
+        'content-type': 'application/x-www-form-urlencoded',
+        'content-type': 'text/plain'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        with open("access_data.json", "w") as access_file:
+            access_file.write(response.text)
+        return response.json()["access_token"]
+    
+    with open("access_data.json", "r") as access_file:
+        access_data = json.loads(access_file.read())
+        return access_data["access_token"]
+
+
+def get_products(access_token):
+    url = "https://api.moltin.com/pcm/products"
+
+    payload={}
+    headers = {
+    'accept': 'application/json',
+    'content-type': 'application/json',
+    'Authorization': f'Bearer {access_token}'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_data = json.loads(response.text)["data"]
+   
+    products = [{
+        'id': product["id"],
+        'name': product["attributes"]["name"]
+    } for product in response_data]
+    return products
+
+
+def get_product(access_token, product_id):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+
+    response = requests.get(f'https://api.moltin.com/pcm/products/{product_id}', headers=headers)
+    product_data = json.loads(response.text)["data"]
+    product = {
+        "name": product_data["attributes"]["name"],
+        "description": product_data["attributes"]["description"],
+        "image": product_data["relationships"]["main_image"]["data"]["id"],
+        "id": product_data["id"]
+    }
+    return product
+
+
+def get_file(access_token, file_id):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(f'https://api.moltin.com/v2/files/{file_id}', headers=headers)
+    return json.loads(response.text)["data"]["link"]["href"]
+
+
+def get_items_in_cart(access_token, chat_id):
+    url = f"https://api.moltin.com/v2/carts/{chat_id}/items"
+
+    headers = {
+        'accept': 'application/json',
+        'content-type': 'application/json',
+        'Authorization': f'Bearer {access_token}'
+        }
+
+    response = requests.request("GET", url, headers=headers)
+
+    response_data = json.loads(response.text)["data"]
+    items_in_cart = [{
+        "id": product["id"],
+        "product_id": product["product_id"],
+        "name": product["name"],
+        "description": product["description"],
+        "quantity": product["quantity"],
+        "price_per_kg": product["meta"]["display_price"]["with_tax"]["unit"]["formatted"],
+        "value": product["meta"]["display_price"]["with_tax"]["value"]["formatted"]
+    } for product in response_data]
+    
+    return items_in_cart
+
+
+def get_total_amount_for_cart(access_token, chat_id):
+    url = f"https://api.moltin.com/v2/carts/{chat_id}"
+
+    headers = {
+        'accept': 'application/json',
+        'content-type': 'application/json',
+        'Authorization': f'Bearer {access_token}'
+        }
+
+    response = requests.request("GET", url, headers=headers)
+    response_data = json.loads(response.text)["data"]
+    return response_data["meta"]["display_price"]["with_tax"]["formatted"]
+
+
+def add_product_to_cart(access_token, chat_id, product_id, quantity):
+    url = f"https://api.moltin.com/v2/carts/{chat_id}/items"
+
+    payload = json.dumps({
+        "data": {
+            "id": f"{product_id}",
+            "type": "cart_item",
+            "quantity": quantity
+        }
+        })
+    headers = {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'Authorization': f'Bearer {access_token}'
+            }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+
+def delete_cart_item(access_token, chat_id, id):
+    url = f"https://api.moltin.com/v2/carts/{chat_id}/items/{id}"
+
+    headers = {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'Authorization': f'Bearer {access_token}'
+            }
+
+    response = requests.request("DELETE", url, headers=headers)
+
+
+def create_customer(access_token, chat_id, email):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'data': {
+            'type': 'customer',
+            'name': str(chat_id),
+            'email': email,
+        }
+    }
+
+    response = requests.post('https://api.moltin.com/v2/customers', headers=headers, json=json_data)
---
+++ tg_bot.py
@@ -0,0 +1,180 @@
+import redis
+import re
+
+from environs import Env
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Filters, Updater, CallbackQueryHandler, CommandHandler, MessageHandler
+
+from elasticpath import get_access_token, get_products, get_product, get_file,\
+      add_product_to_cart, get_items_in_cart, get_total_amount_for_cart,\
+      delete_cart_item, create_customer
+
+
+env = Env()
+env.read_env()
+
+_database = None
+
+
+def show_main_menu():
+    access_token = get_access_token()
+    products = get_products(access_token)
+    keyboard = [[InlineKeyboardButton(product["name"], callback_data=product["id"])] for product in products]
+    keyboard.append([InlineKeyboardButton("Cart", callback_data="cart")])
+    return InlineKeyboardMarkup(keyboard)
+
+
+def show_cart(access_token, chat_id):
+    products_in_cart = get_items_in_cart(access_token, chat_id)
+    total_value = get_total_amount_for_cart(access_token, chat_id)
+    products_in_cart_texts = ["\n".join([
+            product["name"],
+            product["description"],
+            f"{product['price_per_kg']} per kg",
+            f"{product['quantity']}kg for {product['value']}"]) for product in products_in_cart]
+    products_in_cart_texts.append(f"Total: {total_value}")
+    cart_text = "\n\n".join(products_in_cart_texts)
+    buttons = [[InlineKeyboardButton(f"Remove {product['name']}", callback_data=f"{product['id']}")] for product in products_in_cart]
+    buttons.append([InlineKeyboardButton("Pay", callback_data="pay")])
+    buttons.append([InlineKeyboardButton("Back to menu", callback_data="back")])
+    reply_markup = InlineKeyboardMarkup(buttons)
+    return cart_text, reply_markup
+    
+
+def start(update, context):    
+    reply_markup = show_main_menu()
+    update.message.reply_text("Please choose:", reply_markup=reply_markup)
+    
+    return "HANDLE_MENU"
+
+
+def handle_menu(update, context):
+    query = update.callback_query
+    access_token = get_access_token()
+    chat_id = query.message.chat_id
+    menu_msg_id = query.message.message_id
+    if query.data == "cart":
+        context.bot.delete_message(message_id=menu_msg_id, chat_id=chat_id)
+        cart_text, reply_markup = show_cart(access_token, chat_id)
+        context.bot.send_message(text=cart_text, chat_id=chat_id, reply_markup=reply_markup)
+        return "HANDLE_CART"
+    else:
+        product = get_product(access_token, query.data.strip())
+        image_url = get_file(access_token, product["image"])
+        query.answer()
+        
+        context.bot.delete_message(message_id = menu_msg_id, chat_id = chat_id)
+        buttons = [[InlineKeyboardButton("1 kg", callback_data=f"1 {product['id']}"), InlineKeyboardButton("5 kg", callback_data=f"5 {product['id']}")],
+                [InlineKeyboardButton("Back", callback_data="back")]]
+        keyboard_markup = InlineKeyboardMarkup(buttons)
+        context.bot.send_photo(chat_id=chat_id,
+                                photo=image_url,
+                                caption=f"{product['name']}\n{product['description']}",
+                                reply_markup=keyboard_markup)
+        return "HANDLE_DESCRIPTION"
+
+
+def handle_description(update, context):
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    if query.data == "back":
+        menu_msg_id = query.message.message_id
+        context.bot.delete_message(message_id = menu_msg_id, chat_id = chat_id)
+        reply_markup = show_main_menu()
+        context.bot.send_message(text="Please choose", chat_id=query.message.chat_id, reply_markup=reply_markup)
+        
+        return "HANDLE_MENU"
+    else:
+        quantity, product_id = query.data.split()
+        access_token = get_access_token()
+        add_product_to_cart(access_token, chat_id, product_id, int(quantity))
+        return "HANDLE_DESCRIPTION"
+
+
+def handle_cart(update, context):
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    old_msg_id = query.message.message_id
+    context.bot.delete_message(message_id = old_msg_id, chat_id = chat_id)
+    if query.data == "back":
+        reply_markup = show_main_menu()
+        context.bot.send_message(text="Please choose", chat_id=chat_id, reply_markup=reply_markup)
+        return "HANDLE_MENU"
+    if query.data == "pay":
+        context.bot.send_message(text="Please, send us email to contact you", chat_id=chat_id)
+        return "WAITING_EMAIL"
+    else:
+        item_in_cart_id = query.data
+        access_token = get_access_token()
+        delete_cart_item(access_token, chat_id, item_in_cart_id)
+        cart_text, reply_markup = show_cart(access_token, chat_id)
+        context.bot.send_message(chat_id=chat_id, text=cart_text, reply_markup=reply_markup)
+        return "HANDLE_CART"
+
+
+def waiting_email(update, context):
+    users_message = update.message.text
+    chat_id = update.message.chat_id
+
+    regex_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    if re.fullmatch(regex_email, users_message):
+        context.bot.send_message(chat_id=chat_id, text=f"Your email is {users_message}")
+        access_token = get_access_token()
+        create_customer(access_token, chat_id, users_message)
+        reply_markup = show_main_menu()
+        context.bot.send_message(text="Please choose", chat_id=chat_id, reply_markup=reply_markup)
+        return "HANDLE_MENU"
+    
+    context.bot.send_message(chat_id=chat_id, text="Invalid email, try again")
+    return "WAITING_EMAIL"
+
+
+def handle_users_reply(update, context):
+    db = get_database_connection()
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id).decode("utf-8")
+    
+    states_functions = {
+        'START': start,
+        'HANDLE_MENU': handle_menu,
+        'HANDLE_DESCRIPTION': handle_description,
+        'HANDLE_CART': handle_cart,
+        'WAITING_EMAIL': waiting_email
+    }
+    state_handler = states_functions[user_state]
+
+    try:
+        next_state = state_handler(update, context)
+        db.set(chat_id, next_state)
+    except Exception as err:
+        print(err)
+
+def get_database_connection():
+    global _database
+    if _database is None:
+        database_password = env("DATABASE_PASSWORD")
+        database_host = env("DATABASE_HOST")
+        database_port = env("DATABASE_PORT")
+        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
+    return _database
+
+
+if __name__ == '__main__':
+    
+    token = env("TELEGRAM_TOKEN")
+    updater = Updater(token, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    updater.start_polling()
